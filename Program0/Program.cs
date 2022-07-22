92a0be428be7e973a17efa4d8867670ba3acfd88
id3at <tomasz.gluc@gmail.com>
2022-07-22 13:17:43 +0200

    Test0

---
+++ Program0/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+int liczba_wlosowana = rnd.Next(1, 11);
+
+int licznik_prob = 0;
+
+Console.WriteLine("Została wylosowana liczba z zakresu od 1 do 10");
+Console.WriteLine("Zgadnij jaka to liczba !");
+Console.WriteLine("Podaj liczbę:");
+
+string typUzytkownikaStr = Console.ReadLine();
+int typUzytkownikaInt = int.Parse(typUzytkownikaStr);
+
+bool wartownik_petli = true;
+while (wartownik_petli)
+{
+
+
+
+    if (typUzytkownikaInt == liczba_wlosowana)
+    {
+        Console.WriteLine($"Zgadłeś !! Liczba prób wynosi {licznik_prob}");
+        wartownik_petli = false;
+    }
+
+
+    else if (typUzytkownikaInt > liczba_wlosowana)
+    {
+        Console.WriteLine("Liczba jest za duża");
+    }
+    else
+    {
+        Console.WriteLine("Liczba jest za mała");
+    }
+    if (wartownik_petli)
+    {
+        Console.WriteLine("Podaj liczbę:");
+        typUzytkownikaStr = Console.ReadLine();
+
+        typUzytkownikaInt = int.Parse(typUzytkownikaStr);
+
+    }
+    licznik_prob++;
+}
+
+Console.ReadKey();
