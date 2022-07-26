362f8c6f47b66243120b6fe81379a6f018063daa
id3at <tomasz.gluc@gmail.com>
2022-07-26 15:23:05 +0200

    Dodanie konstruktora classy.

--- KalkulatorEnergii/Program.cs
+++ KalkulatorEnergii/Program.cs
@@ -6,21 +6,26 @@ CZas trwania , i koszt.
 */
 
 
+
 KalkulatorEnergii kalk = new KalkulatorEnergii();
+
 
 
 bool wartownik = true;
 do
 {
 
-    Console.WriteLine("Czy chcesz podać własne dane? Wcisnij 'y'. Dane default wciśnij 'n'. Zakończ wciśnij 'q'. ");
+    Console.WriteLine(@"Jeżeli chcesz podać własne dane - Wcisnij 'y' <Enter>. 
+Dane default wciśnij 'n' <Enter>. 
+Zakończ wciśnij 'q' <Enter>.");
     string wyborUzytkownika = Console.ReadLine();
 
     switch (wyborUzytkownika)
 
     {
         case "n":
-            
+
+            Console.Clear();
             Console.WriteLine(kalk.inforamcje());
             Console.ReadKey();
             wartownik = false;
@@ -30,34 +35,26 @@ do
             break;
 
         case "y":
-
+            Console.Clear();
             Console.WriteLine("Podaj cene usługi np. '0,74'");
             kalk.cena_uslugi = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Podaj przeszły odczyt licznika");
             kalk.licznik_od = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Podaj odczyt licznika aktualny");
             kalk.licznik_do = double.Parse(Console.ReadLine());
+
             Console.WriteLine("Podaj jednostkę np. 'kwh'");
             kalk.jednostka = Console.ReadLine();
 
             Console.WriteLine("Podaj date od kiedy '2022,6,1");
             string datkaOd = Console.ReadLine();
-            string[] datkaOdLista = datkaOd.Split(','); 
-            for (int i = 0; i < kalk.data_od.Length; i++)
-            {
-
-                kalk.data_od[i] = int.Parse(datkaOdLista[i]);
-            }
+            kalk.data_od = datkaOd;
 
             Console.WriteLine("Podaj date do kiedy '2022,7,1");
             string datkaDo = Console.ReadLine();
-            string[] datkaDoLista = datkaDo.Split(','); 
-            for (int i = 0; i < kalk.data_do.Length; i++)
-            {
-
-                kalk.data_do[i] = int.Parse(datkaDoLista[i]);
-            }
-
+            kalk.data_do = datkaDo;
 
             Console.WriteLine(kalk.inforamcje());
             Console.ReadKey();
@@ -66,6 +63,7 @@ do
 
         default:
             Console.WriteLine("Musisz wpisać 'y', albo 'n' albo 'q'");
+
             break;
 
     }
@@ -81,27 +79,45 @@ do
 class KalkulatorEnergii
 {
 
+    public double cena_uslugi;
+    public double licznik_od;
+    public double licznik_do;
+    public string jednostka;
 
-    public double cena_uslugi = 0.74;
-    public double licznik_od = 5788 + 236;
-    public double licznik_do = 6433.4;
-    public string jednostka = "kwh";
 
-
-    public int[] data_od = { 2022, 6, 1 };
-    public int[] data_do = { 2022, 7, 24 };
+    public string data_od;
+    public string data_do;
 
     public DateTime dataOd;
     public DateTime dataDo;
 
+    public string[] dataOdString;
+    public string[] dataDoString;
+    public KalkulatorEnergii(string data_od = "2022, 6, 1", 
+                             string data_do = "2022, 7, 26", 
+                             double cena_uslugi = 0.74, 
+                             double licznik_od = 5788 + 236, 
+                             double licznik_do = 6433.4, 
+                             string jednostka = "kwh")
+    {
+        this.cena_uslugi = cena_uslugi;
+        this.licznik_od = licznik_od;
+        this.licznik_do = licznik_do;
+        this.jednostka = jednostka;
+        this.data_od = data_od;
+        this.data_do = data_do;
+
+    }
 
 
 
     public int CzasUzytkownika()
 
     {
-        dataOd = new DateTime(data_od[0], data_od[1], data_od[2]);
-        dataDo = new DateTime(data_do[0], data_do[1], data_do[2]);
+        dataOdString = data_od.Split(',');
+        dataDoString = data_do.Split(',');
+        dataOd = new DateTime(int.Parse(dataOdString[0]), int.Parse(dataOdString[1]), int.Parse(dataOdString[2]));
+        dataDo = new DateTime(int.Parse(dataDoString[0]), int.Parse(dataDoString[1]), int.Parse(dataDoString[2]));
         var roznica = dataDo - dataOd;
         return roznica.Days;
 
@@ -137,10 +153,10 @@ class KalkulatorEnergii
     public string inforamcje()
     {
 
-        return @$"Przez {CzasUzytkownika()} dni zuzyto {Zużycie()} {jednostka}.\n
-Koszt wynosi {Koszt()} zł przy cenie {cena_uslugi} gr.\n
-Średnia zuzycia przez jeden dzień wynosi {ŚredniaJedenDzien()}{jednostka} \n
-Koszt jednego dnia jest równy: {KosztJendegoDnia()}zł przy średniej {ŚredniaJedenDzien()}{jednostka}\n";
+        return @$"Przez {CzasUzytkownika()} dni zuzyto {Zużycie()} {jednostka}.
+Koszt wynosi {Koszt()} zł przy cenie {cena_uslugi} gr.
+Średnia zuzycia przez jeden dzień wynosi {ŚredniaJedenDzien()}{jednostka} 
+Koszt jednego dnia jest równy: {KosztJendegoDnia()}zł przy średniej {ŚredniaJedenDzien()}{jednostka}";
 
     }
 
